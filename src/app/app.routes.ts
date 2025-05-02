670f5b176c3034dbe31132c99322f75db34db8ed
AlvaroTHG <alvarobaldiviezo8@gmail.com>
2025-05-02 14:41:20 -0400

    V1.0.1 - Antes del Desastre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,17 @@ export const routes: Routes = [
   {
     path: 'favorite',
     component: FavoriteComponent,
+  },  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
   },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./favorites/favorites.page').then( m => m.FavoritesPage)
+  },
+  {
+    path: 'pet-detail',
+    loadComponent: () => import('./pet-detail/pet-detail.page').then( m => m.PetDetailPage)
+  },
+
 ];
